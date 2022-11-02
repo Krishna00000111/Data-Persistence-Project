96248e66546053cc3ee8bd4104febc67d2e172f1
Krishna00000111 <86921469+Krishna00000111@users.noreply.github.com>
2022-11-02 15:37:13 +0530

    Data Persistence Project

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,31 +6,29 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    private string displayName;
-
-
     public Brick BrickPrefab;
-
     public int LineCount = 6;
     public Rigidbody Ball;
 
     public Text ScoreText;
-    public Text BestScoreText;
     public GameObject GameOverText;
-    
+    public Text BestScoreText;
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        if (GameManager.Instance != null)
+            BestScoreText.text = "Best Score: " + GameManager.Instance.bestPlayerName + " : " + GameManager.Instance.bestScore;
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -41,8 +39,6 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
-
-        displayName = MenuScene.Instance.playerName;
     }
 
     private void Update()
@@ -72,18 +68,20 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"{m_Points}";
+        ScoreText.text = $"Score : {m_Points}";
     }
 
     public void GameOver()
     {
+        if (GameManager.Instance != null && m_Points > GameManager.Instance.bestScore)
+        {
+            GameManager.Instance.bestScore = m_Points;
+            GameManager.Instance.bestPlayerName = GameManager.Instance.currentPlayerName;
+
+            BestScoreText.text = "Best Score: " + GameManager.Instance.bestPlayerName + " : " + GameManager.Instance.bestScore;
+            GameManager.Instance.SaveData();
+        }
         m_GameOver = true;
         GameOverText.SetActive(true);
-        BestScoreText.text = "Best Score: " + displayName  + ": "+ ScoreText.text ;
-    }
-
-    public void GoBack()
-    {
-        SceneManager.LoadScene(0);
     }
 }
